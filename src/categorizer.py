c6f85ab20cbe417a84e804d2be29f19d7beaeeb1
Brandon Macias <maciasbrandon@protonmail.com>
2026-01-27 18:17:39 -0500

    - Changed "add_category" functions loop method - Added additional technicality to transactions.csv for testing

--- src/categorizer.py
+++ src/categorizer.py
@@ -1,16 +1,30 @@
 def add_category(transaction):
-    categories = {"Food":["grocery", "target", "walmart", "restaurant"], 
+    categories = {"Food":["grocery", "groceries", "target", "walmart", "restaurant"], 
                   "Rent":["rent"], 
                   "Entertainment":["netflix", "hulu", "disney", "amazon prime"],
                   "Utilities":["electricity", "gas", "water", "bill"],
                   "Income":["paycheck", "salary"], 
                   "Other":["miscellaneous", "other", "misc"]}
+    
+    for x in transaction:
+        description = x["description"].lower()
+        for key, value_list in categories.items():
+            if any(word_in_values in description for word_in_values in value_list):
+                x["category"] = key
+                break
+        if "category" not in x:
+            x["category"] = "Other"
 
+
+"""
     for x in transaction:
         description = x["description"].lower()
         for key, value in categories.items():
             if description in value:
                 x["category"] = key
                 break
-            else:
-                x["category"] = "Other"
+        if "category" not in x:
+            x["category"] = "Other" 
+"""
+
+    
