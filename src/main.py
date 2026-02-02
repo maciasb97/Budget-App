b3fbc9192eefd32ae386b67152d3cf19b776928b
Brandon Macias <maciasbrandon@protonmail.com>
2026-02-02 16:39:44 -0500

    - Added import for exporter.py & pandas (main.py) - Added input for passing transaction_totals data to exporter.py (main.py)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import parser
 import categorizer
 import analyzer
+import exporter
+import pandas as pd
 
 data_list = parser.load_transactions()
 categorizer.add_category(data_list)
 transaction_totals = analyzer.calculate(data_list)
+exporter.export_summary(transaction_totals)
 
-print(*data_list, sep='\n')
+
+
+
+
